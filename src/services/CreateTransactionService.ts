639d55f7f649d76a575ff33955e66bd8b4630e4b
Felizardo <devfelizardo@gmail.com>
2020-12-03 02:10:09 -0300

    done is transactions deletion and transaction category creation and balance validation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,11 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 // import repository, because it is the responsible to connect to our database.
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface Request {
   title: string;
@@ -21,12 +22,37 @@ class CreateTransactionService {
     category,
   }: Request): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    //Verify if the balance is positive enought to receive a outcome transaction
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Not enought balance.');
+    }
+
+    //look for the category in order to check if the category already exists.
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    //If the category does not exists, create it in our DB and use the created ID.
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
     //to create new transaction inside our database.
     const transaction = transactionsRepository.create({
       title,
       value,
       type,
-      //category
+      category: transactionCategory,
     });
 
     await transactionsRepository.save(transaction);
